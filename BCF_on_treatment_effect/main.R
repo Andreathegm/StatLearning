4deed40904429141521178c449b4f10a61780b25
Andreathegm <andrea.innocenti13@edu.unifi.it>
2026-06-03 23:46:56 +0200

    Modified main e runbenchmark script to handle the failure of bcf mcmc and divide correctly

--- BCF_on_treatment_effect/main.R
+++ BCF_on_treatment_effect/main.R
@@ -3,13 +3,15 @@ source("run_benchmark.R")
 source("simulation_study.R")
 
 my_real_methods <- list(
-  #"BART"     = run_bart_method,
-  #"ps-BART"  = run_ps_bart_method
-  "BCF"      = run_bcf_method
+  "BCF"      = run_bcf_method,
+  "BART"     = run_bart_method,
+  "ps-BART"  = run_ps_bart_method
 )
 
 set.seed(1234)
-comparison_table <- run_simulation_benchmark(
+
+## heter./linear
+table_heter_linear <- run_simulation_benchmark(
   n_sims = 200, 
   n_obs = 250, 
   effect = "heterogeneous", 
@@ -17,5 +19,46 @@ comparison_table <- run_simulation_benchmark(
   methods_list = my_real_methods,
   dgp = dgp
 )
+saveRDS(table_heter_linear, "results_heter_linear.rds")
+write.csv(table_heter_linear, "results_heter_linear.csv", row.names = TRUE)
 
-print(comparison_table)
+## heter./nonlinear
+table_heter_nonlinear <- run_simulation_benchmark(
+  n_sims = 200, 
+  n_obs = 250, 
+  effect = "heterogeneous", 
+  form = "nonlinear", 
+  methods_list = my_real_methods,
+  dgp = dgp
+)
+saveRDS(table_heter_nonlinear, "results_heter_nonlinear.rds")
+write.csv(table_heter_nonlinear, "results_heter_nonlinear.csv", row.names = TRUE)
+
+## homo./linear
+table_homo_linear <- run_simulation_benchmark(
+  n_sims = 200, 
+  n_obs = 250, 
+  effect = "homogeneous", 
+  form = "linear", 
+  methods_list = my_real_methods,
+  dgp = dgp
+)
+saveRDS(table_homo_linear, "results_homo_linear.rds")
+write.csv(table_homo_linear, "results_homo_linear.csv", row.names = TRUE)
+
+## homo./nonlinear
+table_homo_nonlinear <- run_simulation_benchmark(
+  n_sims = 200, 
+  n_obs = 250, 
+  effect = "homogeneous", 
+  form = "nonlinear", 
+  methods_list = my_real_methods,
+  dgp = dgp
+)
+saveRDS(table_homo_nonlinear, "results_homo_nonlinear.rds")
+write.csv(table_homo_nonlinear, "results_homo_nonlinear.csv", row.names = TRUE)
+
+print(table_heter_linear)
+print(table_heter_nonlinear)
+print(table_homo_linear)
+print(table_homo_nonlinear)
--- BCF_on_treatment_effect/run_benchmark.R
+++ BCF_on_treatment_effect/run_benchmark.R
@@ -1,12 +1,11 @@
 source("evaluate_methods.R")
 
-run_simulation_benchmark <- function(n_sims = 200, n_obs = 250, effect = "heterogeneous", form = "linear", methods_list,dgp) {
+run_simulation_benchmark <- function(n_sims = 200, n_obs = 250, effect = "heterogeneous", form = "linear", methods_list, dgp) {
   
   all_results <- list()
   
   for (i in 1:n_sims) {
-    print(paste0("Simulation",i,"/",n_sims))
- 
+    print(paste0("Simulation ", i, "/", n_sims))
     
     sim_data <- dgp(n = n_obs, effect_type = effect, functional_form = form)
     
@@ -16,16 +15,21 @@ run_simulation_benchmark <- function(n_sims = 200, n_obs = 250, effect = "hetero
     
     for (method_name in names(methods_list)) {
       method_func <- methods_list[[method_name]]
-      
-      model_output <- method_func(sim_data)
-      
-      iter_results[method_name, ] <- evaluate_method(sim_data, model_output)
+      tryCatch({
+        model_output <- method_func(sim_data)
+        iter_results[method_name, ] <- evaluate_method(sim_data, model_output)
+      }, error = function(e) {
+        print(paste0("  Error in ", method_name, " simulation ", i, ": ", e$message))
+      })
     }
     
-    all_results[[i]] <- iter_results
+    all_results[[length(all_results) + 1]] <- iter_results
   }
   
-  final_table <- Reduce("+", all_results) / n_sims
+  n_completed <- length(all_results)
+  print(paste0("Completed simulation: ", n_completed, "/", n_sims))
+  
+  final_table <- Reduce("+", all_results) / n_completed
   
   return(round(final_table, 3))
 }
